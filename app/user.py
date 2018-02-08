496a4680a432c49b741e72b5656e16431208c6a4
liufeiSAP <344501823@qq.com>
2018-02-08 22:46:19 +0800

    add delete update search

--- app/user.py
+++ app/user.py
@@ -17,12 +17,28 @@ def add():
         if not p_user or not p_email or not p_password:
             return 'input error'
 
+        # 增加
         newobj = User(username=p_user, email=p_email, password=p_password)
         db.session.add(newobj)
         db.session.commit()
+
         users = User.query.all()
         return render_template('user/add.html',users=users)
     users = User.query.all()
+
+    # 更改
+    user = db.session.query(User).filter_by(username="ewew").first()
+    user.password = "99999999"
+    db.session.commit()
+
+    # 查询
+    users = db.session.query(User).filter(User.username == 'ewew')
+
+    # 删除
+    user = db.session.query(User).filter_by(username="ewew").first()
+    db.session.delete(user)
+    db.session.commit()
+
     return render_template('hello.html',users=users)
 
 @user.route('/show')
