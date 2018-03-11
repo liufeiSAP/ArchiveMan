a583dd0d35f15e617efa05ae5c6d182924108e06
liufeiSAP <344501823@qq.com>
2018-03-11 13:59:15 +0800

    send email

--- app/service/ArchiveRecordService.py
+++ app/service/ArchiveRecordService.py
@@ -2,7 +2,6 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 
-
 class ArchiveRecordService:
     def snedEmail(self):
         msg = MIMEText('hello,send by python...', 'plain', 'utf-8')
@@ -11,10 +10,9 @@ class ArchiveRecordService:
         msg['To'] = Header('管理员', 'utf-8')
         msg['Subject'] = Header('来自SMTP的问候', 'utf-8')
 
-
         server = smtplib.SMTP("smtp.qq.com", 25)  # SMTP协议默认端口是25
         server.set_debuglevel(1)
         server.starttls()
-        server.login("344501823@qq.com", "slregekgovoocaeg")
+        server.login("344501823@qq.com", "slregekgovoocaeg") #登录QQ邮箱，注意
         server.sendmail("344501823@qq.com", ["344501823@qq.com"], msg.as_string())
         server.quit()
